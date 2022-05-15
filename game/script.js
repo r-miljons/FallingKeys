44247c6104857f30dd40ee2c292ac73ac0997edb
Renars Miljons <reku.miljons@gmail.com>
2022-05-15 21:57:32 +0300

    advance to the next key feature

--- game/script.js
+++ game/script.js
@@ -1,9 +1,10 @@
 // --- game animations that are linked to key press events should check if the game is started!
 // --- spent event listeners should be removed!
 
-const firstKey = document.querySelector(".one");
+const allKeys = document.querySelector(".keys");
 const pressAnyKey = document.querySelector(".start-game");
 const trail = document.querySelector(".trail-box");
+let keysArray = document.querySelectorAll(".key");
 
 // ---------------------------------------------------------------- 
 
@@ -19,7 +20,7 @@ let isPressed; // handles the logic of function execution
 let timeElapsed; // time it took to press the key
 let currentY; // position of the key at the time it was pressed
 
-
+let animationTime = 200; 
 
 //removes idle text and starts falling animation, sets gameStarted = true;
 
@@ -43,11 +44,13 @@ window.addEventListener("keypress", startGame); // removed when function called
 function startFall() {
     setTimeout(() =>{
         if (isPressed) {
-        firstKey.style.opacity = "1";
-        progressTrail(); //spawns a trail
+        keysArray = document.querySelectorAll(".key"); //update the keys array
+        resetClassNames();
+        keysArray[0].style.opacity = "1";
         isPressed = false;
+        progressTrail(); //spawns a trail
         startingTime = Date.now(); // starts timer
-        firstKey.style.animation = "fallDown " + fallingTime + "ms linear forwards";
+        keysArray[0].style.animation = "fallDown " + fallingTime + "ms linear forwards";
         setTimeout(() => { 
             timeElapsed = Date.now() - startingTime;
             if (!isPressed && fallingSpeed * timeElapsed >= 70) { //if key is not pressed
@@ -64,17 +67,15 @@ function pressKey() {
     if (gameStarted) {
         setTimeout(() => {
             if (!isPressed) {
+            changeKeys();
             isPressed = true;
             timeElapsed = Date.now() - startingTime; 
             console.log("press time: " + timeElapsed);
             currentY = fallingSpeed * timeElapsed;
-            firstKey.style.bottom = "-" + currentY + "vh";
-            firstKey.style.animation = "keyPress 0.5s forwards";
+            keysArray[0].style.bottom = "-" + currentY + "vh"; // height where to display the animation
             setTimeout(() => {
-                firstKey.style.animation = "none";
-                firstKey.style.opacity = "0";
-                startFall();
-            }, 500);
+                startFall();   
+            }, animationTime);
         }
         }, 10);
     };
@@ -90,11 +91,43 @@ function progressTrail() {
     let updateTrailLength = setInterval(() => {
         timeElapsed = Date.now() - startingTime;
         currentY = fallingSpeed * timeElapsed;
-        trail.style.height = currentY + "vh";
+        trail.style.height = currentY + 1 +"vh"; // height of the trail (added 1 to keep the end of the trail behind the key)
         if (isPressed) {
             clearInterval(updateTrailLength);
             trail.style.opacity = "0";
             document.querySelector(".trail").style.width = "1vw";
         }
     }, 10);
+}
+
+// advances the key chain
+
+function changeKeys() {
+    keysArray = document.querySelectorAll(".key"); //update the keys array
+    keysArray[0].style.animation = "keyPress "+ animationTime +"ms forwards, slideRight 0.2s forwards linear";
+    allKeys.style.animation = "slideLeft "+ animationTime +"ms forwards linear";
+    setTimeout(() => {
+        clearAnimations();
+        let firstCopy = keysArray[0].cloneNode(true);
+        allKeys.style.animation = "none";
+        keysArray[0].remove();
+        firstCopy.style.bottom = "0"; //reset the height of the copy
+        allKeys.appendChild(firstCopy);    
+    }, animationTime);
+}
+
+function resetClassNames() {
+    keysArray[0].className = "key one";
+    keysArray[1].className = "key two";
+    keysArray[2].className = "key three";
+    keysArray[3].className = "key four";
+    keysArray[4].className = "key five";
+}
+
+function clearAnimations() {
+    keysArray[0].style.animation = "none";
+    keysArray[1].style.animation = "none";
+    keysArray[2].style.animation = "none";
+    keysArray[3].style.animation = "none";
+    keysArray[4].style.animation = "none";
 }
