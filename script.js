6d1b196f7885eef9444e8fe40741ad685a899dec
Renars Miljons <reku.miljons@gmail.com>
2022-05-17 20:56:43 +0300

    improved intro page animations

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ const startBox = document.querySelector(".start-section-container");
 const gitHubLogo = document.querySelector(".project");
 
 function redirectToGame() {
-    setTimeout(() => {window.location.href= "./game/game.html";},1000);
+    setTimeout(() => {
+        window.location.href= "./game/game.html";
+    },1000);
 }
 
 function animatePlayButton() {
     startBox.style.animation = "fallDown 1s forwards";
     gitHubLogo.style.animation = "fadeOut 1s forwards";
 }
+
+// before this ↓ when coming back from game page, this page is empty because animations are set to "forwards" to prevent reappearences
+
+function resetAnimations() {
+    startBox.style.animation = "none";
+    gitHubLogo.style.animation = "none";
+}
+
+window.onload = resetAnimations();
 
 startButton.addEventListener("click",redirectToGame);
 startButtonText.addEventListener("click", redirectToGame);
